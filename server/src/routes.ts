1f0a34d056279f3db3f3008183324118502fa6dd
Juancoted <juandiegocds@gmail.com>
2022-05-08 10:10:29 -0300

    Criando um único repositório para a aplicação

---
+++ server/src/routes.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+import { SubmitFeedbackUseCase } from './use-cases/submitFeedbackUseCase';
+import { PrismaFeedbacksRepository } from './repositories/prisma/prismaFeedbacksRepository'
+import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailerMailAdapter';
+
+export const routes = express();
+
+;
+
+routes.post('/feedbacks', async (req, res) => {
+    const { type, comment, screenshot } = req.body;
+    //possibilita usar a sintaxe curta no objeto data ao inves de req.body.type
+    try {
+        const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+        const nodemailerMailAdapter = new NodemailerMailAdapter()
+
+        const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+            prismaFeedbacksRepository,
+            nodemailerMailAdapter
+        );
+
+        await submitFeedbackUseCase.execute({
+            type,
+            comment,
+            screenshot
+        });
+
+        return res.status(201).send();
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send()
+    }
+
+})
